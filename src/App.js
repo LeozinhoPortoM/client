ae15ee75e5134a938020b287ae1bccf33accbcad
LeozinhoPortoM <leo.porto.lpm@hotmail.com>
2023-03-08 10:03:50 -0300

    inserting routes login, register and home. Included navbar and footer components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import './App.css';
+import "./App.css";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { Navbar } from "./components/Navbar/Navbar";
+import { Footer } from "./components/Footer/Footer";
+
+import { Home } from "../src/pages/Home/Home";
+import { Login } from "./pages/Auth/Login";
+import { Register } from "./pages/Auth/Register";
 
 function App() {
   return (
     <div className="App">
-      <h1>React Gram</h1>
+      <BrowserRouter>
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </main>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
