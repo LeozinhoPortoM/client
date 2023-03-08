c5ba02176a79ddf708923d998a854702de5d8071
LeozinhoPortoM <leo.porto.lpm@hotmail.com>
2023-03-08 10:59:55 -0300

    register form created

--- src/pages/Auth/Register.js
+++ src/pages/Auth/Register.js
@@ -1,9 +1,26 @@
-import "./Auth.css"
+import "./Auth.css";
+
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 export const Register = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
-    <div>
-      <h1>Register</h1>
+    <div id="register">
+      <h2>React Gram</h2>
+      <p>Cadastre-se para ver as fotos dos seus amigos.</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Nome" />
+        <input type="email" placeholder="E-mail" />
+        <input type="password" placeholder="Senha" />
+        <input type="password" placeholder="Confirme a senha" />
+        <button type="submit">Cadastrar</button>
+      </form>
+      <p>
+        Já tem conta? <Link to={"/login"}>Clique aqui.</Link>{" "}
+      </p>
     </div>
   );
 };
