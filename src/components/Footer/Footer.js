cc3dc16ede1291d70e9f44b34d42e2b4e6b4d2b4
LeozinhoPortoM <leo.porto.lpm@hotmail.com>
2023-03-08 09:52:08 -0300

    created footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+export const Footer = () => {
+  return (
+    <footer id="footer">
+      <span>
+        React Gram desenvolvido por <strong>Léo Porto Maranhão</strong> &copy; 2023
+      </span>
+    </footer>
+  );
+};
