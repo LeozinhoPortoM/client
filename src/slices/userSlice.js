2716533c91b1cf59000bcd84213d06b61bad73d2
LeozinhoPortoM <leo.porto.lpm@hotmail.com>
2023-03-29 11:45:50 -0300

    inserted getUserDetails method in redux

--- src/slices/userSlice.js
+++ src/slices/userSlice.js
@@ -33,6 +33,15 @@ export const updateProfile = createAsyncThunk(
   }
 );
 
+export const getUserDetails = createAsyncThunk(
+  "user/get",
+  async (id, thunkAPI) => {
+    const data = await userService.getUserDetails(id);
+
+    return data;
+  }
+);
+
 export const userSlice = createSlice({
   name: "user",
   initialState,
@@ -62,12 +71,22 @@ export const userSlice = createSlice({
         state.success = true;
         state.error = null;
         state.user = action.payload;
-        state.message = "Usuário atualizado com sucesso!"
+        state.message = "Usuário atualizado com sucesso!";
       })
       .addCase(updateProfile.rejected, (state, action) => {
         state.loading = false;
         state.error = action.payload;
         state.user = {};
+      })
+      .addCase(getUserDetails.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(getUserDetails.fulfilled, (state, action) => {
+        state.loading = false;
+        state.success = true;
+        state.error = null;
+        state.user = action.payload;
       });
   },
 });
